ce1d02ce5c9896d17750cb9f2fbae7f0d563fc2b
MbiluM <mabilu@usf.edu>
2025-02-14 22:03:01 -0500

    Customer Invoice Calculation

---
+++ Coding Challange #7.js
@@ -0,0 +1,7 @@
+ // Task 1 - Customer Invoice Calculation
+ 
+ function calculateInvoice(subtotal, taxRate, discount) {
+    retrun = (Subtotal + (subtotal * taxRate)) - discount
+ }
+ console.log(calculateInvoice(100, 0.08, 5));
+ console.log(calculateInvoice(500, 0.1, 20))
